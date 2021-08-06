9564f6fca71f198da67154bf54a896b30bbd5b8d
Lukasz Kolano <lukaszkolano28@gmail.com>
2021-08-06 23:12:07 +0100

    06/08/21

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ new Vue({
             info: null,
             loading: true,
             errored: false,
-            selected: ' ',
+            selected: '',
         };
     },
     mounted() {
@@ -13,7 +13,10 @@ new Vue({
             .get(
                 'https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=36d9b02b3c2716866d6fa0e801dd15d9b837938f'
             )
-            .then(response => (this.info = response.data))
+            .then(response => {
+                this.info = response.data;
+                console.log(this.info);
+            })
             .catch(error => {
                 console.log(error);
                 this.errored = true;
