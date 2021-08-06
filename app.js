e9d3578c3c5bc233126a1baf979efb308608359e
Lukasz Kolano <lukaszkolano28@gmail.com>
2021-08-06 23:53:25 +0100

    06/08/21

--- app.js
+++ app.js
@@ -2,25 +2,30 @@ new Vue({
     el: '#app',
     data() {
         return {
-            info: null,
+            info: {},
             loading: true,
             errored: false,
             selected: '',
         };
     },
+    methods: {
+        async fetchApi() {
+            await axios
+                .get(
+                    'https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=36d9b02b3c2716866d6fa0e801dd15d9b837938f'
+                )
+                .then(response => {
+                    this.info = response.data;
+                    console.log(this.info);
+                })
+                .catch(error => {
+                    console.log(error);
+                    this.errored = true;
+                })
+                .finally(() => (this.loading = false));
+        },
+    },
     mounted() {
-        axios
-            .get(
-                'https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey=36d9b02b3c2716866d6fa0e801dd15d9b837938f'
-            )
-            .then(response => {
-                this.info = response.data;
-                console.log(this.info);
-            })
-            .catch(error => {
-                console.log(error);
-                this.errored = true;
-            })
-            .finally(() => (this.loading = false));
+        this.fetchApi();
     },
 });
